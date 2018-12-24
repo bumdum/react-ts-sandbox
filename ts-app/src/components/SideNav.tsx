ec4a9f9b6c351e0d5ccf9c76db6e6a020f747f4b
bumdum <jared.support@gmail.com>
2018-12-24 16:19:23 -0700

    TopNav SideNav

---
+++ ts-app/src/components/SideNav.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+class SideNav extends React.Component
+{
+
+}
+
+export default SideNav;
---
+++ ts-app/src/components/TopNav.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+class TopNav extends React.Component
+{
+
+}
+
+export default TopNav;
